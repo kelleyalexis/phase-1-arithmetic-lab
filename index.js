f8fa76ce0492041f7ebe029f73d8030e6e6065e7
Kelley Shackelford <kelleyalexis@yahoo.com>
2024-04-18 15:29:16 -0400

    Refactored arithmetic lab

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const num1 = 2;
-const num2 = 31;
-const multiply = num1 * num2;
+let num1 = 2
+let num2 = 31
+const multiply = (num1 * num2); 
 
+    
 const random = Math.floor(Math.random() * 10) + 1;
 
-const num3 = 25
-const num4 = 7
-const mod = num3 % num4;
+let num3 = 25
+let num4 = 7
+const mod = (num3 % num4);
 
-const max = 20
-// Write your code here
+const max = Math.max(2, 14, 9, 20, 0);
